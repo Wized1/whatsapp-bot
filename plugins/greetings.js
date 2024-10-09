91819ab6adc5458dfe69de4129b5567ca9ab98ce
Astro <danielfx016@gmail.com>
2024-10-09 16:15:50 +0100

    fix

--- plugins/greetings.js
+++ plugins/greetings.js
@@ -1,97 +1,94 @@
 const { bot } = require('../utils');
-const { getGreetings, setWelcome, setGoodbye } = require('../lib');
-
-const formatMessage = async (message, user, groupName, client) => {
-  const groupMetadata = await client.groupMetadata(message.jid);
-  const participantCount = groupMetadata.participants.length;
-  const adminCount = groupMetadata.participants.filter(p => p.admin).length;
-
-  return message
-    .replace('@pp', '') // Placeholder for profile picture, handled separately
-    .replace('@user', `@${user.split('@')[0]}`)
-    .replace('@gname', groupName)
-    .replace('@count', participantCount.toString())
-    .replace('@admins', adminCount.toString());
-};
+const GreetingsHandler = require('../utils/whatsapp/Greetings');
 
 bot(
-  {
-    pattern: 'welcome ?(.*)',
-    fromMe: true,
-    desc: 'Set welcome message',
-    type: 'group',
-  },
-  async (message, match, m, client) => {
-    if (!message.isGroup) return message.reply('This command is only for groups.');
+ {
+  pattern: 'welcome',
+  fromMe: true,
+  desc: 'Manage welcome messages',
+  type: 'group',
+ },
+ async (message, match) => {
+  if (!message.isGroup) return await message.reply('This command can only be used in groups.');
 
-    const [cmd, ...args] = match.trim().split(' ');
-    const welcomeMessage = args.join(' ');
+  let { prefix } = message;
+  let status = await GreetingsHandler.getGreetingStatus(message.jid, 'welcome');
+  let stat = status ? 'on' : 'off';
 
-    if (cmd === 'on' || cmd === 'off') {
-      const enabled = cmd === 'on';
-      await setWelcome(message.jid, null, enabled);
-      return message.reply(`Welcome message ${enabled ? 'enabled' : 'disabled'}.`);
-    }
-
-    if (!welcomeMessage) {
-      const greetings = await getGreetings(message.jid);
-      return message.reply(`Current welcome message: ${greetings.welcomeMessage}\nStatus: ${greetings.welcomeEnabled ? 'Enabled' : 'Disabled'}`);
-    }
-
-    await setWelcome(message.jid, welcomeMessage, true);
-    return message.reply('Welcome message set and enabled.');
+  if (!match) {
+   let replyMsg = `Welcome Message Manager\n\nGroup: ${(await message.client.groupMetadata(message.jid)).subject}\nStatus: ${stat}\n\nAvailable Actions:\n\n- ${prefix}welcome get: Get the current welcome message\n- ${prefix}welcome on: Enable welcome message\n- ${prefix}welcome off: Disable welcome message\n- ${prefix}welcome delete: Delete the welcome message\n- ${prefix}welcome <text>: Set a new welcome message`;
+   return await message.reply(replyMsg);
   }
+
+  switch (match.split(' ')[0].toLowerCase()) {
+   case 'get':
+    let msg = await GreetingsHandler.getGreetingMessage(message.jid, 'welcome');
+    if (!msg) return await message.reply('_There is no welcome message set_');
+    return message.reply(`Current welcome message:\n\n${msg.message}`);
+
+   case 'on':
+    if (status) return await message.reply('_Welcome message is already enabled_');
+    await GreetingsHandler.toggleGreetingStatus(message.jid, 'welcome');
+    return await message.reply('_Welcome message has been enabled_');
+
+   case 'off':
+    if (!status) return await message.reply('_Welcome message is already disabled_');
+    await GreetingsHandler.toggleGreetingStatus(message.jid, 'welcome');
+    return await message.reply('_Welcome message has been disabled_');
+
+   case 'delete':
+    await GreetingsHandler.deleteGreeting(message.jid, 'welcome');
+    return await message.reply('_Welcome message has been deleted successfully_');
+
+   default:
+    await GreetingsHandler.setGreeting(message.jid, 'welcome', match);
+    return await message.reply('_Welcome message has been set successfully_');
+  }
+ }
 );
 
 bot(
-  {
-    pattern: 'goodbye ?(.*)',
-    fromMe: true,
-    desc: 'Set goodbye message',
-    type: 'group',
-  },
-  async (message, match, m, client) => {
-    if (!message.isGroup) return message.reply('This command is only for groups.');
+ {
+  pattern: 'goodbye',
+  fromMe: true,
+  desc: 'Manage goodbye messages',
+  type: 'group',
+ },
+ async (message, match) => {
+  if (!message.isGroup) return await message.reply('This command can only be used in groups.');
 
-    const [cmd, ...args] = match.trim().split(' ');
-    const goodbyeMessage = args.join(' ');
+  let { prefix } = message;
+  let status = await GreetingsHandler.getGreetingStatus(message.jid, 'goodbye');
+  let stat = status ? 'on' : 'off';
 
-    if (cmd === 'on' || cmd === 'off') {
-      const enabled = cmd === 'on';
-      await setGoodbye(message.jid, null, enabled);
-      return message.reply(`Goodbye message ${enabled ? 'enabled' : 'disabled'}.`);
-    }
-
-    if (!goodbyeMessage) {
-      const greetings = await getGreetings(message.jid);
-      return message.reply(`Current goodbye message: ${greetings.goodbyeMessage}\nStatus: ${greetings.goodbyeEnabled ? 'Enabled' : 'Disabled'}`);
-    }
-
-    await setGoodbye(message.jid, goodbyeMessage, true);
-    return message.reply('Goodbye message set and enabled.');
+  if (!match) {
+   let replyMsg = `Goodbye Message Manager\n\nGroup: ${(await message.client.groupMetadata(message.jid)).subject}\nStatus: ${stat}\n\nAvailable Actions:\n\n- ${prefix}goodbye get: Get the current goodbye message\n- ${prefix}goodbye on: Enable goodbye message\n- ${prefix}goodbye off: Disable goodbye message\n- ${prefix}goodbye delete: Delete the goodbye message\n- ${prefix}goodbye <text>: Set a new goodbye message`;
+   return await message.reply(replyMsg);
   }
-);
 
-bot(
-  {
-    on: 'group_update',
-    fromMe: false,
-  },
-  async (message, match, m, client) => {
-    if (message.action === 'add') {
-      const greetings = await getGreetings(message.jid);
-      if (greetings.welcomeEnabled) {
-        const groupName = message.subject || 'the group';
-        const formattedMessage = await formatMessage(greetings.welcomeMessage, message.participants[0], groupName, client);
-        await client.sendMessage(message.jid, { text: formattedMessage, mentions: message.participants });
-      }
-    } else if (message.action === 'remove') {
-      const greetings = await getGreetings(message.jid);
-      if (greetings.goodbyeEnabled) {
-        const groupName = message.subject || 'the group';
-        const formattedMessage = await formatMessage(greetings.goodbyeMessage, message.participants[0], groupName, client);
-        await client.sendMessage(message.jid, { text: formattedMessage, mentions: message.participants });
-      }
-    }
+  switch (match.split(' ')[0].toLowerCase()) {
+   case 'get':
+    let msg = await GreetingsHandler.getGreetingMessage(message.jid, 'goodbye');
+    if (!msg) return await message.reply('_There is no goodbye message set_');
+    return message.reply(`Current goodbye message:\n\n${msg.message}`);
+
+   case 'on':
+    if (status) return await message.reply('_Goodbye message is already enabled_');
+    await GreetingsHandler.toggleGreetingStatus(message.jid, 'goodbye');
+    return await message.reply('_Goodbye message has been enabled_');
+
+   case 'off':
+    if (!status) return await message.reply('_Goodbye message is already disabled_');
+    await GreetingsHandler.toggleGreetingStatus(message.jid, 'goodbye');
+    return await message.reply('_Goodbye message has been disabled_');
+
+   case 'delete':
+    await GreetingsHandler.deleteGreeting(message.jid, 'goodbye');
+    return await message.reply('_Goodbye message has been deleted successfully_');
+
+   default:
+    await GreetingsHandler.setGreeting(message.jid, 'goodbye', match);
+    return await message.reply('_Goodbye message has been set successfully_');
   }
+ }
 );
