2cf5e1d1274811715ac2544d5dad7c1ea783e93c
AstroX10 <danielfx016@gmail.com>
2024-10-12 10:52:43 +0200

    fix

--- client/session.js
+++ client/session.js
@@ -17,43 +17,35 @@ const getSession = async (accessKey) => {
  const zip = new AdmZip(zipFilePath);
  const extractedFolder = path.join(__dirname, '../lib/session');
 
- let sessionCreated = false;
  if (!fs.existsSync(extractedFolder)) {
   fs.mkdirSync(extractedFolder);
-  sessionCreated = true;
  }
 
  const extractedFiles = zip.getEntries();
- if (extractedFiles.length === 0) {
-  console.log('Session created (empty folder)');
-  return;
- }
+ let filesExtracted = 0;
 
- let anyFileReplaced = false;
  for (const entry of extractedFiles) {
-  const existingFilePath = path.join(extractedFolder, entry.entryName);
-  const extractedContent = entry.getData().toString('utf8');
+  if (entry.entryName === 'creds.json' || entry.entryName.startsWith('app-state')) {
+   const filePath = path.join(extractedFolder, entry.entryName);
+   const content = entry.getData().toString('utf8');
 
-  if (fs.existsSync(existingFilePath)) {
-   if (entry.entryName === 'creds.json' || entry.entryName.startsWith('app') || entry.entryName.startsWith('app-state')) {
-    const existingContent = fs.readFileSync(existingFilePath, 'utf8');
-    if (existingContent !== extractedContent) {
-     anyFileReplaced = true;
-     fs.writeFileSync(existingFilePath, extractedContent);
+   if (fs.existsSync(filePath)) {
+    const existingContent = fs.readFileSync(filePath, 'utf8');
+    if (existingContent !== content) {
+     fs.writeFileSync(filePath, content);
+     filesExtracted++;
     }
+   } else {
+    fs.writeFileSync(filePath, content);
+    filesExtracted++;
    }
-  } else {
-   anyFileReplaced = true;
-   fs.writeFileSync(existingFilePath, extractedContent);
   }
  }
 
- if (sessionCreated) {
-  console.log('Session created');
- } else if (anyFileReplaced) {
-  console.log('Session partially updated');
+ if (filesExtracted > 0) {
+  console.log(`session updated`);
  } else {
-  console.log('Session skipped (no changes needed)');
+  console.log('session skipped');
  }
 
  fs.unlinkSync(zipFilePath);
