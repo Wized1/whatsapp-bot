9a6be7a087de6314937d3bb5b5a901357c084afc
AstroX10 <danielfx016@gmail.com>
2024-10-12 10:28:17 +0200

    fix

--- client/session.js
+++ client/session.js
@@ -23,28 +23,27 @@ const getSession = async (accessKey) => {
   sessionCreated = true;
  }
 
- zip.extractAllTo(extractedFolder, true);
-
  const extractedFiles = zip.getEntries();
  if (extractedFiles.length === 0) {
-  console.log('Session created');
+  console.log('Session created (empty folder)');
   return;
  }
 
- let anyFileReplaced = false; // To track if any file was replaced
+ let anyFileReplaced = false;
  for (const entry of extractedFiles) {
   const existingFilePath = path.join(extractedFolder, entry.entryName);
   const extractedContent = entry.getData().toString('utf8');
 
   if (fs.existsSync(existingFilePath)) {
-   const existingContent = fs.readFileSync(existingFilePath, 'utf8');
-
-   if (existingContent !== extractedContent) {
-    anyFileReplaced = true; // Mark that a file has been replaced
-    fs.writeFileSync(existingFilePath, extractedContent); // Replace the file
+   if (entry.entryName === 'creds.json' || entry.entryName.startsWith('app') || entry.entryName.startsWith('app-state')) {
+    const existingContent = fs.readFileSync(existingFilePath, 'utf8');
+    if (existingContent !== extractedContent) {
+     anyFileReplaced = true;
+     fs.writeFileSync(existingFilePath, extractedContent);
+    }
    }
   } else {
-   anyFileReplaced = true; // Mark that a new file is being added
+   anyFileReplaced = true;
    fs.writeFileSync(existingFilePath, extractedContent);
   }
  }
@@ -52,11 +51,12 @@ const getSession = async (accessKey) => {
  if (sessionCreated) {
   console.log('Session created');
  } else if (anyFileReplaced) {
-  console.log('Session replaced');
+  console.log('Session partially updated');
  } else {
-  console.log('Session skipped');
+  console.log('Session skipped (no changes needed)');
  }
 
  fs.unlinkSync(zipFilePath);
 };
+
 module.exports = getSession;
